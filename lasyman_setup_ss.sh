c0e3154135264c0af03e27db2c712d0fc40a937e
jccg <logic90@qq.com>
2018-04-05 15:35:18 +0800

    Update lasyman_setup_ss.sh

--- lasyman_setup_ss.sh
+++ lasyman_setup_ss.sh
@@ -29,7 +29,7 @@ RESET=1
 CHECK_OS_VERSION=`cat /etc/issue |sed -n 1"$1"p|awk '{printf $1}' |tr 'a-z' 'A-Z'`
 
 #list the software need to be installed to the variable FILELIST
-UBUNTU_TOOLS_LIBS="python-pip python-m2crypto git supervisor \
+UBUNTU_TOOLS_LIBS="htop nload python-pip python-m2crypto git supervisor \
 				 language-pack-zh*"
 
 CENTOS_TOOLS_LIBS="php55w php55w-opcache mysql55w mysql55w-server php55w-mysql php55w-gd libjpeg* \
